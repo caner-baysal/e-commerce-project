770c8da3396a2cc799e1b5261b1607c218b9d6ed
caner-baysal <canerbaysal@hotmail.com>
2026-01-28 13:05:06 +0300

    3rd commit-hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [isOpen, setIsOpen] = useState(false);
+  
   return (
-    <>
-      
-    </>
+    <div className='app'>
+      <header className='header'>
+        <div className='logo'>Bandage</div>
+        <div className='icons'>
+          <button className='icon-btn'>🔍</button>
+          <button className='icon-btn'>🛒</button>
+          <button className='icon-btn' onClick={() => setIsOpen(!isOpen)}>☰</button>
+        </div>
+      </header>
+      <nav className={`mobile-nav ${isOpen ? `open` : ``}`}>
+        <a href="#">Home</a>
+        <a href="#">Shop</a>
+        <a href="#">Product</a>
+        <a href="#">Pricing</a>
+        <a href="#">Pages</a>
+        <a href="#">Blog</a>
+        <a href="#">About</a>
+        <a href="#">Contact</a>
+      </nav>
+
+      <main>
+        <section className="hero" style={{display: isOpen ? 'none' : 'block'}}>
+          <div className='hero-content'>
+            <button className='shop-now-btn'></button>
+          </div>
+        </section>
+      </main>
+    </div>
   )
 }
 
 export default App
+
+
